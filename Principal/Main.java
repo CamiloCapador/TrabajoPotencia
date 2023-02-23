499eac82c87dc2151d974f9f428735a6f1a23a83
CamiloCapador <juanka1307@gmail.com>
2023-02-23 10:51:43 -0500

    Commit potencia y raiz cuadrada

--- Principal/Main.java
+++ Principal/Main.java
@@ -3,19 +3,26 @@ import java.util.Random;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("\nCalcular Potencia entre dos números");
-        System.out.println("- Numero mayor elevado en el menor\n");
+        System.out.println("Ejericios Math");
+        System.out.println("\nCalcular Potencia");
+        System.out.println("- Número mayor elevado en el menor\n");
 
         Operacion oper = new Operacion();
         Random ale = new Random();
 
         int num1, num2;
+        double num3;
         num1 = ale.nextInt(20);
         num2 = ale.nextInt(20);
+        num3 = ale.nextDouble(100.0);
 
         oper.setNumero1(num1);
         oper.setNumero2(num2);
+        oper.setNumraiz(num3);
 
         System.out.println("Número 1: "+oper.getNumero1()+"\nNúmero 2: "+oper.getNumero2()+"\nPotencia: "+oper.calcularPotencia(oper.getNumero1(), oper.getNumero2()));
+        System.out.println("\nCalcular Raiz");
+        System.out.println("- Raiz de número mayor entre n´mero menor\n");
+        System.out.println("La raiz cuadrada de: "+oper.getNumraiz()+" es: "+oper.calcularRaiz(oper.getNumraiz()));
     }
 }
--- Principal/Operacion.java
+++ Principal/Operacion.java
@@ -5,14 +5,16 @@ import static java.lang.Math.pow;
 public class Operacion {
     private int numero1;
     private int numero2;
+    private double numraiz;
 
     public Operacion() {
 
     }
 
-    public Operacion(int numero1, int numero2) {
+    public Operacion(int numero1, int numero2, double numraiz) {
         this.numero1 = numero1;
         this.numero2 = numero2;
+        this.numraiz = numraiz;
     }
 
     public int getNumero1() {
@@ -31,11 +33,20 @@ public class Operacion {
         this.numero2 = numero2;
     }
 
+    public double getNumraiz() {
+        return numraiz;
+    }
+
+    public void setNumraiz(double numraiz) {
+        this.numraiz = numraiz;
+    }
+
     @Override
     public String toString() {
         return "Operacion{" +
                 "numero1=" + numero1 +
                 ", numero2=" + numero2 +
+                ", numraiz=" + numraiz +
                 '}';
     }
 
@@ -45,5 +56,9 @@ public class Operacion {
         } else {
             return (int) Math.pow(num2, num1);
         }
+    }
+
+    public double calcularRaiz(double num3){
+        return (double) Math.sqrt(num3);
     }
 }
